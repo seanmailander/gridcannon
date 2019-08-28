2d30c71060e49bf0e64da444e48ac739d2fcbf1b
Sean Mailander <me@seanmailander.com>
2019-08-27 20:08:53 -0700

    Show the deck

--- docs/game.js
+++ docs/game.js
@@ -1,167 +1,168 @@
 import {
-  shuffleDeck, isRoyalty,
+    shuffleDeck, isRoyalty,
 } from './deck.js';
-import { drawGrid, drawCurrentCard } from './view.js';
+import { drawGrid, drawDeck, drawCurrentCard } from './view.js';
 
 import { actions } from './game.actions.js';
 import { howManyCardsPlaced, targetsFiredUpon } from './game.selectors.js';
 
 const initialState = () => {
-  const newDeck = shuffleDeck();
-  const topCard = newDeck.shift();
-  return {
-    deckInHand: newDeck,
-    currentCard: topCard,
-    skippedRoyalty: [],
-    grid: [...Array(25)].map(() => []),
-  };
+    const newDeck = shuffleDeck();
+    const topCard = newDeck.shift();
+    return {
+        deckInHand: newDeck,
+        currentCard: topCard,
+        skippedRoyalty: [],
+        grid: [...Array(25)].map(() => []),
+    };
 };
 
 const applyStateChange = (action = {}, state = initialState()) => {
-  switch (action.type) {
+    switch (action.type) {
     case actions.RESET_GAME: {
-      return initialState();
+        return initialState();
     }
     case actions.SET_ROYALTY_ASIDE: {
-      const { skippedRoyalty, deckInHand, currentCard } = state;
+        const { skippedRoyalty, deckInHand, currentCard } = state;
 
-      const copiedDeck = deckInHand.slice();
-      // take nextcard out of deck
-      const nextCard = copiedDeck.shift();
+        const copiedDeck = deckInHand.slice();
+        // take nextcard out of deck
+        const nextCard = copiedDeck.shift();
 
-      // return new state
-      return {
-        ...state,
-        // put current card in royalty stack
-        skippedRoyalty: [...skippedRoyalty, currentCard],
-        // take next card out of deck
-        deckInHand: copiedDeck,
-        // show next card from top of deck
-        currentCard: nextCard,
+        // return new state
+        return {
+            ...state,
+            // put current card in royalty stack
+            skippedRoyalty: [...skippedRoyalty, currentCard],
+            // take next card out of deck
+            deckInHand: copiedDeck,
+            // show next card from top of deck
+            currentCard: nextCard,
 
-      };
+        };
     }
     case actions.PLACE_CARD_DURING_DEAL: {
-      const {
-        grid, deckInHand, skippedRoyalty, currentCard,
-      } = state;
-      const { position } = action;
+        const {
+            grid, deckInHand, skippedRoyalty, currentCard,
+        } = state;
+        const { position } = action;
 
-      // put card in grid
-      // stacks on!
-      const newGrid = grid.slice();
-      newGrid[position].unshift(currentCard);
+        // put card in grid
+        // stacks on!
+        const newGrid = grid.slice();
+        newGrid[position].unshift(currentCard);
 
-      // was this the last grid fill?
-      // that means there are currently 7 cards placed, and this is 8
-      const gridIsAboutToFill = howManyCardsPlaced(state) === 7;
-      const thereIsSkippedRoyalty = skippedRoyalty.length > 0;
-      if (gridIsAboutToFill && thereIsSkippedRoyalty) {
+        // was this the last grid fill?
+        // that means there are currently 7 cards placed, and this is 8
+        const gridIsAboutToFill = howManyCardsPlaced(state) === 7;
+        const thereIsSkippedRoyalty = skippedRoyalty.length > 0;
+        if (gridIsAboutToFill && thereIsSkippedRoyalty) {
         // next card is top of royalty
-        const copiedRoyalty = skippedRoyalty.slice();
-        // take next card out of royalty
-        const nextCard = copiedRoyalty.shift();
+            const copiedRoyalty = skippedRoyalty.slice();
+            // take next card out of royalty
+            const nextCard = copiedRoyalty.shift();
+
+            // return new state
+            return {
+                ...state,
+                // put current card in grid
+                grid: newGrid,
+                // no change to deck in hand
+                deckInHand,
+                // take next card out of royalty
+                skippedRoyalty: copiedRoyalty,
+                // show next card from top of royalty
+                currentCard: nextCard,
+            };
+        }
+        // next card is top of deck
+        const copiedDeck = deckInHand.slice();
+        // take next card out of deck
+        const nextCard = copiedDeck.shift();
 
         // return new state
         return {
-          ...state,
-          // put current card in grid
-          grid: newGrid,
-          // no change to deck in hand
-          deckInHand,
-          // take next card out of royalty
-          skippedRoyalty: copiedRoyalty,
-          // show next card from top of royalty
-          currentCard: nextCard,
+            ...state,
+            // put current card in grid
+            grid: newGrid,
+            // take next card out of deck in hand
+            deckInHand: copiedDeck,
+            // no change to royalty
+            skippedRoyalty,
+            // show next card from top of deck
+            currentCard: nextCard,
         };
-      }
-      // next card is top of deck
-      const copiedDeck = deckInHand.slice();
-      // take next card out of deck
-      const nextCard = copiedDeck.shift();
-
-      // return new state
-      return {
-        ...state,
-        // put current card in grid
-        grid: newGrid,
-        // take next card out of deck in hand
-        deckInHand: copiedDeck,
-        // no change to royalty
-        skippedRoyalty,
-        // show next card from top of deck
-        currentCard: nextCard,
-      };
     }
     case actions.PLAY_CARD: {
-      const {
-        grid, deckInHand, skippedRoyalty, currentCard,
-      } = state;
-      const { position } = action;
+        const {
+            grid, deckInHand, skippedRoyalty, currentCard,
+        } = state;
+        const { position } = action;
 
-      // put card in grid
-      // stacks on!
-      const newGrid = grid.slice();
-      newGrid[position].unshift(currentCard);
+        // put card in grid
+        // stacks on!
+        const newGrid = grid.slice();
+        newGrid[position].unshift(currentCard);
 
-      // check for trigger
-      if (!isRoyalty(currentCard)) {
-        const destroyedPositions = targetsFiredUpon(position, newGrid);
-        destroyedPositions.forEach((destroyPos) => newGrid[destroyPos].push({ destroyed: true }));
-      }
+        // check for trigger
+        if (!isRoyalty(currentCard)) {
+            const destroyedPositions = targetsFiredUpon(position, newGrid);
+            destroyedPositions.forEach((destroyPos) => newGrid[destroyPos].push({ destroyed: true }));
+        }
 
-      // is there more royalty?
-      const isMoreRoyalty = skippedRoyalty.length > 0;
-      if (isMoreRoyalty) {
+        // is there more royalty?
+        const isMoreRoyalty = skippedRoyalty.length > 0;
+        if (isMoreRoyalty) {
         // next card is top of royalty
-        const copiedRoyalty = skippedRoyalty.slice();
-        // take next card out of royalty
-        const nextCard = copiedRoyalty.shift();
+            const copiedRoyalty = skippedRoyalty.slice();
+            // take next card out of royalty
+            const nextCard = copiedRoyalty.shift();
+
+            // return new state
+            return {
+                ...state,
+                // put current card in grid
+                grid: newGrid,
+                // no change to deck in hand
+                deckInHand,
+                // take next card out of royalty
+                skippedRoyalty: copiedRoyalty,
+                // show next card from top of royalty
+                currentCard: nextCard,
+            };
+        }
+        // next card is top of deck
+        const copiedDeck = deckInHand.slice();
+        // take next card out of deck
+        const nextCard = copiedDeck.shift();
 
         // return new state
         return {
-          ...state,
-          // put current card in grid
-          grid: newGrid,
-          // no change to deck in hand
-          deckInHand,
-          // take next card out of royalty
-          skippedRoyalty: copiedRoyalty,
-          // show next card from top of royalty
-          currentCard: nextCard,
+            ...state,
+            // put current card in grid
+            grid: newGrid,
+            // take next card out of deck in hand
+            deckInHand: copiedDeck,
+            // no change to royalty
+            skippedRoyalty,
+            // show next card from top of deck
+            currentCard: nextCard,
         };
-      }
-      // next card is top of deck
-      const copiedDeck = deckInHand.slice();
-      // take next card out of deck
-      const nextCard = copiedDeck.shift();
-
-      // return new state
-      return {
-        ...state,
-        // put current card in grid
-        grid: newGrid,
-        // take next card out of deck in hand
-        deckInHand: copiedDeck,
-        // no change to royalty
-        skippedRoyalty,
-        // show next card from top of deck
-        currentCard: nextCard,
-      };
     }
     default:
-      return state;
-  }
+        return state;
+    }
 };
 
 export default function getInstance() {
-  let state = initialState();
+    let state = initialState();
 
-  return (thunk) => {
-    state = applyStateChange(thunk(state), state);
-    drawGrid(state);
-    drawCurrentCard(state);
+    return (thunk) => {
+        state = applyStateChange(thunk(state), state);
+        drawGrid(state);
+        drawDeck(state);
+        drawCurrentCard(state);
 
-    return state;
-  };
+        return state;
+    };
 }
--- docs/view.js
+++ docs/view.js
@@ -1,93 +1,106 @@
 import {
-  getCardAsUnicode,
-  getSuitAsClassname,
+    getCardAsUnicode,
+    getSuitAsClassname,
 } from './deck.js';
 
 import {
-  whatLegalMoves,
+    whatLegalMoves,
 } from './game.selectors.js';
 import { targetSpots } from './game.consts.js';
 
 export const setupGrid = () => {
-  const grid = document.getElementById('grid');
-  [...Array(25)].forEach((element, i) => {
-    const cardSpot = document.createElement('div');
-    cardSpot.id = `spot${i}`;
-    cardSpot.className = 'cardSpot';
-    grid.appendChild(cardSpot);
-  });
+    const grid = document.getElementById('grid');
+    [...Array(25)].forEach((element, i) => {
+        const cardSpot = document.createElement('div');
+        cardSpot.id = `spot${i}`;
+        cardSpot.className = 'cardSpot';
+        grid.appendChild(cardSpot);
+    });
+};
+
+export const drawDeck = (state) => {
+    const cardElement = document.getElementById('deck');
+    [...cardElement.childNodes].forEach((node) => cardElement.removeChild(node));
+
+    const {
+        deckInHand,
+    } = state;
+    if (deckInHand && deckInHand.length > 0) {
+        const text = document.createTextNode(getCardAsUnicode(null, null, true));
+        cardElement.appendChild(text);
+    }
 };
 
 export const drawCurrentCard = (state) => {
-  const cardElement = document.getElementById('currentCard');
-  [...cardElement.childNodes].forEach((node) => cardElement.removeChild(node));
+    const cardElement = document.getElementById('currentCard');
+    [...cardElement.childNodes].forEach((node) => cardElement.removeChild(node));
 
-  const {
-    currentCard,
-  } = state;
-  if (currentCard) {
     const {
-      suit,
-      card,
-    } = currentCard;
-    const text = document.createTextNode(getCardAsUnicode(suit, card));
-    cardElement.appendChild(text);
-    cardElement.className = `${getSuitAsClassname(suit)}`;
-  }
+        currentCard,
+    } = state;
+    if (currentCard) {
+        const {
+            suit,
+            card,
+        } = currentCard;
+        const text = document.createTextNode(getCardAsUnicode(suit, card));
+        cardElement.appendChild(text);
+        cardElement.className = `${getSuitAsClassname(suit)}`;
+    }
 };
 
 export const drawGrid = (state) => {
-  const {
-    grid,
-  } = state;
-  const legalMoves = whatLegalMoves(state);
-  grid.forEach((stack, index) => {
-    const spot = document.getElementById(`spot${index}`);
-    [...spot.childNodes].forEach((node) => spot.removeChild(node));
+    const {
+        grid,
+    } = state;
+    const legalMoves = whatLegalMoves(state);
+    grid.forEach((stack, index) => {
+        const spot = document.getElementById(`spot${index}`);
+        [...spot.childNodes].forEach((node) => spot.removeChild(node));
 
-    const isLegal = legalMoves.indexOf(index) !== -1;
-    const isRoyal = targetSpots.indexOf(index) === -1;
-    const hasCard = stack.length > 0;
-    const hasStack = (stack.length > 1);
+        const isLegal = legalMoves.indexOf(index) !== -1;
+        const isRoyal = targetSpots.indexOf(index) === -1;
+        const hasCard = stack.length > 0;
+        const hasStack = (stack.length > 1);
 
-    if (hasCard) {
-      if (isRoyal) {
-        const {
-          suit,
-          card,
-          destroyed = false,
-        } = stack[stack.length - 1];
-        if (destroyed) {
-          const text = document.createTextNode(getCardAsUnicode(suit, card, destroyed));
-          spot.appendChild(text);
-          spot.className = 'cardSpot';
+        if (hasCard) {
+            if (isRoyal) {
+                const {
+                    suit,
+                    card,
+                    destroyed = false,
+                } = stack[stack.length - 1];
+                if (destroyed) {
+                    const text = document.createTextNode(getCardAsUnicode(suit, card, destroyed));
+                    spot.appendChild(text);
+                    spot.className = 'cardSpot';
+                } else {
+                    const text = document.createTextNode(getCardAsUnicode(suit, card));
+                    spot.appendChild(text);
+                    const armorValue = stack.reduce((acc, curr) => acc + curr.card, -card);
+                    spot.className = `cardSpot ${getSuitAsClassname(suit)} ${isLegal ? 'legal' : ''} ${hasStack ? `armor${armorValue}` : ''}`;
+                }
+            } else {
+                const {
+                    suit,
+                    card,
+                } = stack[0];
+                const text = document.createTextNode(getCardAsUnicode(suit, card));
+                spot.appendChild(text);
+                spot.className = `cardSpot ${getSuitAsClassname(suit)} ${isLegal ? 'legal' : ''} ${hasStack ? 'stack' : ''}`;
+            }
         } else {
-          const text = document.createTextNode(getCardAsUnicode(suit, card));
-          spot.appendChild(text);
-          const armorValue = stack.reduce((acc, curr) => acc + curr.card, -card);
-          spot.className = `cardSpot ${getSuitAsClassname(suit)} ${isLegal ? 'legal' : ''} ${hasStack ? `armor${armorValue}` : ''}`;
+            spot.className = `cardSpot ${isLegal ? 'legal' : ''}`;
         }
-      } else {
-        const {
-          suit,
-          card,
-        } = stack[0];
-        const text = document.createTextNode(getCardAsUnicode(suit, card));
-        spot.appendChild(text);
-        spot.className = `cardSpot ${getSuitAsClassname(suit)} ${isLegal ? 'legal' : ''} ${hasStack ? 'stack' : ''}`;
-      }
-    } else {
-      spot.className = `cardSpot ${isLegal ? 'legal' : ''}`;
-    }
-  });
+    });
 };
 
 export const attachToInterface = (handlers) => {
-  const restartBtn = document.getElementById('restartBtn');
-  restartBtn.addEventListener('click', handlers.restart);
+    const restartBtn = document.getElementById('restartBtn');
+    restartBtn.addEventListener('click', handlers.restart);
 
-  [...Array(25)].forEach((element, index) => {
-    const spot = document.getElementById(`spot${index}`);
-    spot.addEventListener('click', () => handlers.placeCard(index));
-  });
+    [...Array(25)].forEach((element, index) => {
+        const spot = document.getElementById(`spot${index}`);
+        spot.addEventListener('click', () => handlers.placeCard(index));
+    });
 };
