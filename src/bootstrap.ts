c3031e80a130700d9f7b5168fda0d32ce6134385
Sean Mailander <seanmailander@users.noreply.github.com>
2021-08-29 23:01:38 -0700

    Lint fix

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,9 +1,9 @@
 import './libs/polyfills.ts';
 
-import onLoad from "./app/app.ts";
-import { $on } from "./ui/helpers.ts";
+import onLoad from './app/app.ts';
+import { $on } from './ui/helpers.ts';
 
-import "./ui/styles.css";
+import './ui/styles.css';
 
-$on(window, "load", onLoad);
-$on(window, "hashchange", onLoad);
+$on(window, 'load', onLoad);
+$on(window, 'hashchange', onLoad);
