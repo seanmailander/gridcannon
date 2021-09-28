8d5906b263ff80d72805ac0dd1f21e7fa19b3b33
Sean Mailander <seanmailander@users.noreply.github.com>
2021-09-28 13:03:51 -0700

    Chore/Add better tests (#25)
    * Better test coverage
    
    * Lint exceptions
    
    * Add placeholder tests
    
    * Lint fixes
    
    * Reenable game

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import {
-  setupGrid,
-  attachToInterface,
-  drawGrid,
-  drawDeck,
-  drawCurrentCard,
-  changeHint,
+    setupGrid,
+    attachToInterface,
+    drawGrid,
+    drawDeck,
+    drawCurrentCard,
+    changeHint,
 } from "../ui/view";
 
 import setInstructions from "../ui/instructions";
@@ -13,16 +13,7 @@ import { RESET_GAME, LOAD_TEST_STATE } from "./game.reducer";
 
 import { howManyCardsPlaced } from "./game.selectors";
 import {
-  noRoyalsOnDeal,
-  alreadyWon,
-  closeToAWin,
-  midGameArmor,
-  noCardsLeft,
-  doubleTrigger,
-  aboutToWin,
-  canWeDoIt,
-  addingArmor,
-  unwinnableArmor,
+    unwinnableArmor,
 } from "./game.test-states";
 
 import { store } from "./store";
@@ -30,52 +21,50 @@ import { store } from "./store";
 const { dispatch, getState } = store;
 
 const dealGrid = () => {
-  let placedCards = 0;
-  // Place grid one-by-one
-  while (placedCards < 8) {
-    dispatch(dealNextCard(placedCards));
-    const state = getState();
-    placedCards = howManyCardsPlaced(state);
-  }
+    let placedCards = 0;
+    // Place grid one-by-one
+    while (placedCards < 8) {
+        dispatch(dealNextCard(placedCards));
+        const state = getState();
+        placedCards = howManyCardsPlaced(state);
+    }
 };
 
 const cardSpotClicked = (position) => {
-  dispatch(tryToPlayCard(position));
+    dispatch(tryToPlayCard(position));
 };
 
 const restartGame = () => {
-  dispatch(RESET_GAME());
-  dealGrid();
+    dispatch(RESET_GAME());
+    dealGrid();
 };
 
 const logStateToConsole = () => {
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  const noopAction = () => {};
-  const currentState = dispatch(noopAction);
-  // eslint-disable-next-line no-console
-  console.debug(JSON.stringify(currentState));
-  // console.debug(LZString.compressToBase64(JSON.stringify(dispatch(() => {}))));
+    const currentState = getState();
+    // eslint-disable-next-line no-console
+    console.debug(JSON.stringify(currentState));
+    // console.debug(LZString.compressToBase64(JSON.stringify(currentState)));
 };
 
 const loadState = () => {
-  dispatch(LOAD_TEST_STATE(unwinnableArmor));
+    dispatch(LOAD_TEST_STATE(unwinnableArmor));
 };
 
 export default function onLoad() {
-  setupGrid();
-  attachToInterface({
-    restart: restartGame,
-    placeCard: cardSpotClicked,
-    saveState: logStateToConsole,
-    loadState,
-  });
-  const unsubscribe = store.subscribe(() => {
-    const state = store.getState();
-    drawGrid(state);
-    drawDeck(state);
-    drawCurrentCard(state);
-    changeHint(state);
-    setInstructions(state);
-  });
-  restartGame();
+    setupGrid();
+    attachToInterface({
+        restart: restartGame,
+        placeCard: cardSpotClicked,
+        saveState: logStateToConsole,
+        loadState,
+    });
+    const unsubscribe = store.subscribe(() => {
+        const state = store.getState();
+        drawGrid(state);
+        drawDeck(state);
+        drawCurrentCard(state);
+        changeHint(state);
+        setInstructions(state);
+    });
+    restartGame();
 }
